58798dd3982fc952b4bc114deeda256b6ff8268e
Alan Christian <alan@upmarck.com>
2023-05-03 15:49:46 +0200

    Added a few more comments

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,8 @@
     <div>
         Write the login component here!
         <!-- Intructions -->
-        <!-- 1. Add the logo. Use the SVG version. I have created the component for the logo. Using this approach keeps the code clean -->
+        <!-- 0. To access this component use the link: http://localhost:5173/login -->
+        <!-- 1. Add the logo. Use the SVG version. I have created the component for the logo. With this approach the logo has been turned into a reusable component. -->
         <logo></logo>
         <!-- 2. Add the text and the text boxes -->
         <!-- 3. Add the button -->
