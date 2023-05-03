3a2072ce371a0c2361d95ef2cdd2aa216b30b20e
Alan Christian <alan@upmarck.com>
2023-05-03 15:35:20 +0200

    Added instructions in on how to write the login component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-        This is Login
+        Write the login component here!
+        <!-- Intructions -->
+        <!-- 1. Add the logo. Use the SVG version. I have created the component for the logo. Using this approach keeps the code clean -->
+        <logo></logo>
+        <!-- 2. Add the text and the text boxes -->
+        <!-- 3. Add the button -->
     </div>
 </template>
 <script>
+import logo from "../components/icons/Logo.vue"
+
 export default{
-    
+    components:{
+        logo
+    }    
 }
 </script>
 <style>
