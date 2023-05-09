a41f3a725086188faf62d08c6328ff2114aac950
sammowbray <124808491+sammowbray@users.noreply.github.com>
2023-05-09 13:44:04 +0200

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,19 @@
     <div>
         This is Login
     </div>
+    <!-- Email button -->
+    <div class="flex flex-row justify-center items-center p-4 gap-4 absolute left-16.8% right-17.07% top-33.87% bottom-60.59% bg-gray-400 shadow-md rounded-full w-248 h-10">
+        <input type="text" placeholder="E-mail" class="placeholder-white bg-transparent outline-none">
+    </div>
+    <!-- Password button -->
+    <div class="flex flex-row justify-center items-center p-4 gap-4 absolute left-16.8% right-17.07% top-50% bottom-44% bg-gray-400 shadow-md rounded-full w-248 h-10 mt-14">
+        <input type="text" placeholder="Password" class="placeholder-white bg-transparent outline-none">
+    </div>
+    <!-- Login button -->
+    <div class="flex flex-row justify-center items-center p-4 gap-10 absolute left-32.27% right-32.27% top-64 bottom-30 bg-black rounded-full w-1/4">
+        <p class="text-white items-center font-">Login</p>
+    </div>
+
 </template>
 <script>
 export default{
