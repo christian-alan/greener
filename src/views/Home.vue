a3ecbf28f3032b824307a157da82f60dd68baa94
Alan Christian <alan@upmarck.com>
2023-05-11 17:15:42 +0200

    Commit before pull

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
-    <div class="flex justify-between flex-col h-full">
+    <div class="flex justify-between flex-col h-full common-container">
         <!-- Appbar -->
         <AppBarVue></AppBarVue>
 
+
+
         
         <!--Body -->
-        <div class="h-full">
+        <div class="h-full common-container">
             <!--Greeting -->
-            <h1 class="text-2xl font-bold p-4">Welcome back, <br> Rosanna</h1>
+            <h1 ref="heading" class="text-2xl font-bold p-4">Welcome back, <br> Rosanna</h1>
+
+
+             <!-- Dummy -->
+            <!-- <div class="flex w-full flex-col">
+                <div>
+                    <button class="bg-pink-500 rounded-md p-1 text-white" @click="changeText()">Change text</button>
+                </div>
+                <p ref="greeting">Hi</p>
+            </div>  -->
+
             <!--This week's banner -->
-            <div class="w-full py-8 px-4 flex items-center justify-between" style="background-color:#CBDDD2;">
+            <div class="w-full py-8 px-4 flex items-center justify-between" style="background-color:#E0E0E0;">
                 <div>
                     <p>This week’s seasonal vegetable is</p>
                     <p class="font-bold">Beatroot</p>
@@ -22,11 +34,11 @@
 
 
              <!--This month's Seasonal items -->
-             <div class="w-full py-8 px-4 flex flex-col" style="background-color:#F1F4F2;">
+             <div class="w-full py-4 px-4 flex flex-col" style="background-color:#F1F4F2;">
            
                 <p class="font-bold text-sm leading-5 mb-4">This months top seasonal items</p>
                 <div class="card p-4 flex flex-row">
-                    <img src=".././assets/images/apple.png" width="122" height="90" alt="beat root">
+                    <img :src="`src/assets/images/${showFruits}`" width="122" height="90" alt="beat root">
                     <div class="ml-2">
                         <p class="font-bold text-xl">Apple</p>
                         <p class="text-xs opacity-50">Type: Aroma</p>
@@ -39,7 +51,7 @@
 
 
              <!--This week's Recipes -->
-             <div class="w-full py-8 px-4 flex items-center justify-between" style="background-color:#F1F4F2;">
+             <div class="w-full py-8 px-4 flex items-center justify-between" style="background-color:#CBDDD2;">
                 <div>
                     <p class="font-bold text-sm leading-5">Interested to know any <br> recipes  for seasonal foods</p>
                     <p class="text-sm underline opacity-30">We have an endless selection</p>
@@ -51,7 +63,8 @@
             </div>
         </div>
 
-         
+
+       
 
         <BottomBarVue></BottomBarVue>
 
@@ -64,7 +77,55 @@ export default{
     components:{
         AppBarVue,
         BottomBarVue
+    },
+    methods:{
+     changeFruits(){
+            let counter = 0; 
+            setInterval(() =>{ 
+                this.showFruits = this.fruits[counter];
+                counter++; 
+                if(counter === 4){
+                    counter = 0;
+                }
+
+            },1000)
+        },
+
+        changeText(){
+            this.element.innerText = "Hey Hows it going!"
+            this.heading.innerText = "Hey Alan, hope you are doing well";
+        }
+    },
+   mounted(){
+
+      this.element = this.$refs.greeting;
+      this.heading = this.$refs.heading;
+    
+      this.changeFruits();
+
+
+
+    },
+
+    computed:{
+        imagePath() {
+             return `@/assets/images/${this.showFruits}`;
+        },
+    },
+    data(){
+        return {
+            heading:"",
+            element: "",
+            showFruits:"apple.png",
+            fruits:[
+                "apple.png",
+                "grapes.png",
+                "strawberries.png",
+                "redcabbage.png"
+            ]
+        }
     }
+ 
 }
 </script>
 <style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,10 +2,46 @@
     <div>
         This is Login
     </div>
+
+    <div>
+        <input type="text" v-model="newTodo">
+        <button @click="addTodo()">Add Btn</button>
+    </div>
+    
+    <pre>
+        {{ todos }}
+    </pre>
+    
+
+
+
 </template>
 <script>
 export default{
-    
+    data(){
+        return {
+            id:0,
+            newTodo:'',
+            todos:[
+                {
+                    id:this.id++,
+                    text:'hey'
+                }
+            ]
+        }
+    },
+    methods:{
+        addTodo(){
+            console.log(this.newTodo);
+            this.todos.push({id:this.id++,text:this.newTodo});
+        },
+        removeTodo(){
+
+        }
+    },
+    async created(){
+        
+    }
 }
 </script>
 <style>
