31ea995dfb18f804f833b962062b9419d7defcb0
tojva <92752073+tojva@users.noreply.github.com>
2023-06-02 13:18:37 +0200

    Worked a bit

---
+++ src/components/recipe/Ingredient.vue
@@ -0,0 +1,11 @@
+<template>
+
+<div class="flex">
+        <div>      
+          <p class="bg-darkgreen border-2 border-white p-2 rounded-full text-white font-bold pl-4 pr-4">This month</p>
+        </div>
+       </div>
+       
+</template>
+<script></script>
+<style></style>
